731fe30de6e586cecda69b13a21f6a9b71faa060
maxloh <35831069+maxloh@users.noreply.github.com>
2018-12-31 23:26:50 +0800

    Change addAnimation() conditions

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ if ($(window).width() >= 768) {
     $(window).scroll(function () {
         /* Options for scroll tracking statements:
         1. if(document.getElementsByClassName('active').length != 0)
-        2. var navbar = $('#navbar').offset().top;
+        2. let navbar = $('#navbar').offset().top;
            if ($(window).scrollTop() >= navbar)*/
         /*if one of the .nav-link is active, meaning that navbar is sticking to top of the page*/
         if ($('.active')[0]) {
@@ -26,7 +26,7 @@ if ($(window).width() >= 768) {
 window.addEventListener("hashchange", function () {
     /*If animation of that element has not been completed yet*/
     if ($(window.location.hash).css('opacity') < 1) {
-        let translateY = $(window.location.hash).css('transform').replace(/[^0-9\-.,]/g, '').split(',')[5];
+        let translateY = $(window.location.hash).css('transform').match(/matrix.*, (\d+)\)/)[1];
         window.scrollTo(window.scrollX, $(window.location.hash).offset().top - $('#navbar').innerHeight() - parseInt($(':root').css('--row-margin')) - translateY);
     }
     /*If animation of that element has been completed*/
@@ -56,14 +56,18 @@ $(document).ready(function () {
 //CSS animation
 
 /*Set elementList as a globel varieable to improve efficiency (only modifiy once)*/
-var elementList = [];
+var elementList;
 var delayms = 0;
 
 /*Initialize elementList and call addAnimation() on windows load complete*/
 $(document).ready(function () {
-    /*If element with id #navbar exists*/
-    if ($('#navbar')[0]) elementList = $('#navbar, main>.row');
-    else elementList = $('main>.row');
+    /*If element with id #navbar exists, add animation to #navbar (for reloading page while $(window).scrollTop() != 0)*/
+    if ($('#navbar')[0]) {
+        $('#navbar').css({ 'transition-duration': '1250ms', 'opacity': '1', 'transform': 'translateY(0)' });
+        setTimeout(function () { $('#navbar').css({ 'transition-duration': '250ms', 'transition-timing-function': 'ease' }); }, 1);
+        delayms += 250;
+    }
+    elementList = $('main>.row');
     addAnimation();
 });
 
@@ -72,15 +76,14 @@ $(window).scroll(function () { addAnimation() });
 
 /*Add CSS animation class to main .row (for CSS animation)*/
 function addAnimation() {
+    let displayBottom = $(window).scrollTop() + $(window).height();
+    let displayTop = ($('#navbar')[0]) ? $(window).scrollTop() + $('#navbar').innerHeight() : $(window).scrollTop();
     elementList = jQuery.grep(elementList, function (row) {
-        if ($(window).scrollTop() + $(window).height() > $(row).offset().top) {
+        /*if (display bootom > elememnt top && display top < element bottom)*/
+        if (displayBottom > $(row).offset().top && displayTop < $(row).offset().top + $(row).innerHeight() - $(row).css('transform').match(/matrix.*, (\d+)\)/)[1]) {
             setTimeout(function () {
                 delayms -= 250;
-                $(row).css({ 'transition-duration': '1250ms', 'opacity': '1' });
-                $(row).css({ 'transform': 'translateY(0)' });
-                /*Reset #navbar animation duration*/
-                if ($(row).attr('id') === 'navbar')
-                    setTimeout(function () { $('#navbar').css({ 'transition-duration': '250ms', 'transition-timing-function': 'ease-in-out' }); }, 1250);
+                $(row).css({ 'transition-duration': '1250ms', 'opacity': '1', 'transform': 'translateY(0)' });
             }, delayms);
             delayms += 250;
             /*Remove row element from elementList*/
