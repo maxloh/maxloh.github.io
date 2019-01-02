maxloh/maxloh.github.io

commit 0c4f1aeaad6de47ff0de15d9d605cb3e739c96e5

Change navbar margin-left modification implementation

assets/js/main.js
@@ -10,7 +10,7 @@ if ($(window).width() >= 768) {
            if ($(window).scrollTop() >= navbar)*/
         /*if one of the .nav-link is active, meaning that navbar is sticking to top of the page*/
         if ($('.active')[0]) {
-            $('#navbar').css("margin-left", "calc(-15px)");
+            $('#navbar').css("margin-left", "0px");
         } else {
             $('#navbar').css("margin-left", "calc(-15px - 1rem)");
         }

commit 1cf190a5ca040ebbc424b920a1349c14f08349f3

Fix issue caused by 0c4f1ae
https://github.com/maxloh/maxloh.github.io/commit/0c4f1aeaad6de47ff0de15d9d605cb3e739c96e5

assets/js/main.js
@@ -10,7 +10,7 @@ if ($(window).width() >= 768) {
            if ($(window).scrollTop() >= navbar)*/
         /*if one of the .nav-link is active, meaning that navbar is sticking to top of the page*/
         if ($('.active')[0]) {
-            $('#navbar').css("margin-left", "0px");
+            $('#navbar').css("margin-left", "-15px");
         } else {
             $('#navbar').css("margin-left", "calc(-15px - 1rem)");
         }