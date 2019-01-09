ee7028b4eaeeebf70c5ed1340accb8d57823c2f2
maxloh <35831069+maxloh@users.noreply.github.com>
2019-01-10 01:38:01 +0800

    Add PhotoSwipe support, rename elementList to sectionList

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,25 +23,30 @@ if ($(window).width() >= 768) {
 
 
 //Offset jump links (html anchors) for the sticky navbar and amination, pointing them to the correct position
+
 window.addEventListener('hashchange', function () {
-    /*If animation of that element has not been completed yet*/
-    if ($(window.location.hash).css('opacity') < 1) {
-        let currentTranslateY = $(window.location.hash).css('transform').match(/matrix\(.*, (\d*\.?\d+)\)/)[1];
-        window.scrollTo(window.scrollX, $(window.location.hash).offset().top - $('#navbar').innerHeight() - parseInt($(':root').css('--section-margin')) - currentTranslateY);
+    /*If hashchange is not caused by opening PhotoSwipe gallery && hashchange is not caused by closing PhotoSwipe gallery*/
+    if (!window.location.hash.includes('gid') && window.location.hash !== '') {
+        /*If animation of that element has not been completed yet*/
+        if ($(window.location.hash).css('opacity') < 1) {
+            let currentTranslateY = $(window.location.hash).css('transform').match(/matrix\(.*, (\d*\.?\d+)\)/)[1];
+            window.scrollTo(window.scrollX, $(window.location.hash).offset().top - $('#navbar').innerHeight() - parseInt($(':root').css('--section-margin')) - currentTranslateY);
+        }
+        /*If animation of that element has been completed*/
+        else {
+            window.scrollTo(window.scrollX, $(window.location.hash).offset().top - $('#navbar').innerHeight() - parseInt($(':root').css('--section-margin')));
+        }
+        /*Fix scrollspy not accurate after clicking jump links in mobile site*/
+        if ($(window).width() < 768) window.scrollTo(window.scrollX, window.scrollY + 2);
+        /*Remove #id from URL*/
+        history.replaceState(null, null, ' ');
     }
-    /*If animation of that element has been completed*/
-    else {
-        window.scrollTo(window.scrollX, $(window.location.hash).offset().top - $('#navbar').innerHeight() - parseInt($(':root').css('--section-margin')));
-    }
-    /*Fix scrollspy not accurate after clicking jump links in mobile site*/
-    if ($(window).width() < 768) window.scrollTo(window.scrollX, window.scrollY + 2);
-    /*Remove #id from URL*/
-    history.replaceState(null, null, ' ');
 });
 
 
 
 //Initialize bootstrap scrollspy with calculated data-offset
+
 $(document).ready(function () {
     /*If element with id #navbar exists*/
     if ($('#navbar')[0]) {
@@ -54,12 +59,11 @@ $(document).ready(function () {
 
 //CSS animation
 
-/*Set elementList as a globel varieable to improve efficiency (only modifiy once)*/
-var elementList;
+var sectionList;
 var delayTime = 0;
 var delay = parseInt($(':root').css('--section-margin')) * 5;
 
-/*Initialize elementList and call addAnimation() on windows load complete*/
+/*Initialize sectionList and call addAnimation() on windows load complete*/
 $(document).ready(function () {
     /*If element with id #navbar exists, add animation to #navbar (for reloading page while $(window).scrollTop() != 0)*/
     if ($('#navbar')[0]) {
@@ -67,18 +71,19 @@ $(document).ready(function () {
         setTimeout(function () { $('#navbar').css({ 'transition-duration': '250ms', 'transition-timing-function': 'ease' }); }, 1);
         delayTime += delay;
     }
-    elementList = $('main>.section');
+    sectionList = $('main>.section');
     addAnimation();
-});
 
-/*Call addAnimation() method on page scroll (for CSS animation)*/
-$(window).scroll(function () { addAnimation() });
+    /*Call addAnimation() method on page scroll (for CSS animation)
+      $(window).scroll may be fired accidentally when refreshing the page while window.scrollY != 0*/
+    $(window).scroll(function () { addAnimation() });
+});
 
 /*Add CSS animation class to main .section (for CSS animation)*/
 function addAnimation() {
     let displayBottom = $(window).scrollTop() + $(window).height();
     let displayTop = ($('#navbar')[0]) ? $(window).scrollTop() + $('#navbar').innerHeight() : $(window).scrollTop();
-    elementList = jQuery.grep(elementList, function (section) {
+    sectionList = jQuery.grep(sectionList, function (section) {
         /*if (display bootom > elememnt top && display top < element bottom)*/
         if (displayBottom > $(section).offset().top && displayTop < $(section).offset().top + $(section).innerHeight() - $(section).css('transform').match(/matrix\(.*, (\d*\.?\d+)\)/)[1]) {
             setTimeout(function () {
@@ -86,10 +91,10 @@ function addAnimation() {
                 $(section).css({ 'transition-duration': '1250ms', 'opacity': '1', 'transform': 'translateY(0)' });
             }, delayTime);
             delayTime += delay;
-            /*Remove section element from elementList*/
+            /*Remove section element from sectionList*/
             return false;
         }
-        /*Keep section element in elementList as it is not yet being scrolled through*/
+        /*Keep section element in sectionList as it is not yet being scrolled through*/
         else return true;
     });
 }
