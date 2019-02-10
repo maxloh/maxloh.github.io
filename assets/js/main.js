13adda6195c97b93745203c599a5fca8262365fe
maxloh <35831069+maxloh@users.noreply.github.com>
2019-02-11 01:59:52 +0800

    Use MutationObserver to track navbar changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,14 +21,22 @@ else {
 //Let text of .nav-item stick to the edge of its parent element (.container), when navbar is not sticky
 
 if (device.type === 'desktop') {
-    /*When user scroll*/
-    window.addEventListener('scroll', function () {
-        /*if one of the .nav-link is active, meaning that navbar is sticking to top of the page*/
-        if ($('#navbar .nav-link.active')[0]) {
-            $('#navbar .nav-pills').css('transform', 'translateX(1rem)');
-        } else {
-            $('#navbar .nav-pills').css('transform', '');
-        }
+    $(document).ready(function() {
+        /*When user scroll*/
+        var navbarObserver = new MutationObserver(function() {
+            console.log('a');
+            /*if one of the .nav-link is active, meaning that navbar is sticking to top of the page*/
+            if ($('a.nav-link.active')[0]) {
+                $('ul.nav.nav-pills').css('padding-left', '15px');
+            } else {
+                $('ul.nav.nav-pills').css('padding-left', '');
+            }
+        });
+
+        navbarObserver.observe(document.querySelector('li.nav-item:first-child>a.nav-link'), {
+            attributes: true,
+            attributeFilter: ['class']
+        });
     });
 }
 
@@ -36,19 +44,25 @@ if (device.type === 'desktop') {
 
 //Offset jump links (html anchors) for the sticky navbar and amination, pointing them to the correct position
 
-$(document).ready(function () {
-    $('#navbar a').on('click', function () {
+$(document).ready(function() {
+    $('#navbar a').on('click', function() {
         event.preventDefault();
         let target = (this.href).substring((this.href).lastIndexOf('#'));
         let scrollY = $(target).offset().top - $('#navbar').innerHeight() - parseInt($(':root').css('--section-margin'))
 
         if ($(target).css('opacity') < 1) {
             let currentTranslateY = $(target).css('transform').match(/matrix\(.*, (\d*\.?\d+)\)/)[1];
-            window.scroll({ top: scrollY - currentTranslateY + device.scrollOffset, behavior: 'smooth' });
+            window.scroll({
+                top: scrollY - currentTranslateY + device.scrollOffset,
+                behavior: 'smooth'
+            });
         }
         /*If animation of that element has been completed*/
         else {
-            window.scroll({ top: scrollY + device.scrollOffset, behavior: 'smooth' });
+            window.scroll({
+                top: scrollY + device.scrollOffset,
+                behavior: 'smooth'
+            });
         }
     });
 });
@@ -57,11 +71,14 @@ $(document).ready(function () {
 
 //Initialize bootstrap scrollspy with calculated data-offset
 
-$(document).ready(function () {
+$(document).ready(function() {
     /*If element with id #navbar exists*/
     if ($('#navbar')[0]) {
         let dataOffset = $('#navbar').innerHeight() + parseInt($(':root').css('--section-margin')) + parseInt($(':root').css('--section-translateY'));
-        $('body').scrollspy({ target: '#navbar', offset: dataOffset });
+        $('body').scrollspy({
+            target: '#navbar',
+            offset: dataOffset
+        });
     }
 });
 
@@ -69,11 +86,11 @@ $(document).ready(function () {
 
 //CSS animation
 
-const observer = new IntersectionObserver((entries, observer) => {
+const animationObserver = new IntersectionObserver((entries, animationObserver) => {
     entries.forEach((entry) => {
         /*Animate element when it enter viewport*/
         if (entry.intersectionRatio > 0) {
-            observer.unobserve(entry.target);
+            animationObserver.unobserve(entry.target);
             animate(entry.target);
         }
     });
@@ -82,16 +99,20 @@ var delayTime = 0;
 var delay = parseInt($(':root').css('--section-margin')) * 5;
 
 /*observe 'main>.section' and animate #navbar on page load*/
-$(document).ready(function () {
+$(document).ready(function() {
     /*If element with id #navbar exists, animate it*/
     if ($('#navbar')[0]) animate('#navbar');
-    document.querySelectorAll('main>.section').forEach((element) => observer.observe(element));
+    document.querySelectorAll('main>.section').forEach((element) => animationObserver.observe(element));
 });
 
 function animate(target) {
-    setTimeout(function () {
+    setTimeout(function() {
         delayTime -= delay;
-        $(target).css({ 'transition-duration': '1250ms', 'opacity': '1', 'transform': 'translateY(0)' });
+        $(target).css({
+            'transition-duration': '1250ms',
+            'opacity': '1',
+            'transform': 'translateY(0)'
+        });
     }, delayTime);
     delayTime += delay;
 }
