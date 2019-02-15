36682b84ae4956660a8759f987efb41626757fd7
maxloh <35831069+maxloh@users.noreply.github.com>
2019-02-15 23:11:32 +0800

    Format main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,37 @@
 //CSS animation
 try {
-const animationObserver = new IntersectionObserver(function (entries, animationObserver) {
-    entries.forEach(function (entry) {
-        /*Animate element when it enter viewport*/
-        if (entry.intersectionRatio > 0) {
-            animationObserver.unobserve(entry.target);
-            animate(entry.target);
-        }
-    });
-});
-var delayTime = 0;
-var delay = parseInt($(':root').css('--section-margin')) * 5;
-
-/*observe 'main>.section' and animate #navbar on page load*/
-$(document).ready(function () {
-    /*If element with id #navbar exists, animate it*/
-    if ($('#navbar')[0]) animate('#navbar');
-    document.querySelectorAll('main>.section').forEach(function (element) {
-        animationObserver.observe(element);
-    });
-});
-
-function animate(target) {
-    setTimeout(function () {
-        delayTime -= delay;
-        $(target).css({
-            'transition-duration': '1250ms',
-            'opacity': '1',
-            'transform': 'translateY(0)'
+    const animationObserver = new IntersectionObserver(function (entries, animationObserver) {
+        entries.forEach(function (entry) {
+            /*Animate element when it enter viewport*/
+            if (entry.intersectionRatio > 0) {
+                animationObserver.unobserve(entry.target);
+                animate(entry.target);
+            }
         });
-    }, delayTime);
-    delayTime += delay;
-}
+    });
+    var delayTime = 0;
+    var delay = parseInt($(':root').css('--section-margin')) * 5;
+
+    /*observe 'main>.section' and animate #navbar on page load*/
+    $(document).ready(function () {
+        /*If element with id #navbar exists, animate it*/
+        if ($('#navbar')[0]) animate('#navbar');
+        document.querySelectorAll('main>.section').forEach(function (element) {
+            animationObserver.observe(element);
+        });
+    });
+
+    function animate(target) {
+        setTimeout(function () {
+            delayTime -= delay;
+            $(target).css({
+                'transition-duration': '1250ms',
+                'opacity': '1',
+                'transform': 'translateY(0)'
+            });
+        }, delayTime);
+        delayTime += delay;
+    }
 }
 
 //Add partial support for old browsers that do not support IntersectionObserver
