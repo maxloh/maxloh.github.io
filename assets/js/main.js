4437f36de2076af462341da4d0b185a08d3e41f5
maxloh <35831069+maxloh@users.noreply.github.com>
2019-02-18 17:08:34 +0800

    FIx animation not delaying, add animation support for old devices

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,57 @@
 //CSS animation
+const animationCSS = { 'transition-duration': '1250ms', 'opacity': '1', 'transform': 'translateY(0)' };
+const delay = 400;
+
 try {
     const animationObserver = new IntersectionObserver(function (entries, animationObserver) {
         entries.forEach(function (entry) {
             /*Animate element when it enter viewport*/
             if (entry.intersectionRatio > 0) {
                 animationObserver.unobserve(entry.target);
-                animate(entry.target);
+                $(entry.target).css(animationCSS);
             }
         });
     });
-    var delayTime = 0;
-    var delay = 500;
 
     /*observe 'main>.section' and animate #navbar on page load*/
     $(document).ready(function () {
         /*If element with id #navbar exists, animate it*/
-        if ($('#navbar')[0]) animate('#navbar');
-        document.querySelectorAll('main>.section').forEach(function (element) {
-            animationObserver.observe(element);
-        });
-    });
-
-    function animate(target) {
+        if ($('#navbar')[0]) $('#navbar').css(animationCSS);
         setTimeout(function () {
-            delayTime -= delay;
-            $(target).css({
-                'transition-duration': '1250ms',
-                'opacity': '1',
-                'transform': 'translateY(0)'
+            document.querySelectorAll('main>.section').forEach(function (element) {
+                animationObserver.observe(element);
             });
-        }, delayTime);
-        delayTime += delay;
-    }
+        }, delay);
+    });
 }
 //Add partial support for old browsers that do not support IntersectionObserver
 catch (exception) {
-    console.log('Animation disabled\n' + exception);
-    document.addEventListener("DOMContentLoaded", function () {
-        var navbar = document.querySelector('.navbar');
-        navbar.style.opacity = 1;
-        navbar.style.transform = "translateY(0)";
-        [].forEach.call(document.querySelectorAll('main>.section'), function (element) {
-            element.style.opacity = 1;
-            element.style.transform = "translateY(0)";
-        });
+    console.log('Compatibility mode, IntersectionObserver not supported');
+
+    var sectionList;
+
+    $(document).ready(function () {
+        if ($('#navbar')[0]) {
+            $('#navbar').css(animationCSS);
+        }
+        sectionList = $('main>.section');
+        setTimeout(addAnimation, delay);
     });
+    $(window).scroll(addAnimation);
+
+    function addAnimation() {
+        let displayBottom = $(window).scrollTop() + $(window).height();
+        let displayTop = ($('#navbar')[0]) ? $(window).scrollTop() + $('#navbar').innerHeight() : $(window).scrollTop();
+
+        sectionList = jQuery.grep(sectionList, function (section) {
+            let sectionTranslateY = $(section).css('transform').match(/matrix\(.*, (\d*\.?\d+)\)/)[1];
+            let sectionTop = $(section).offset().top - sectionTranslateY;
+            let sectionBottom = sectionTop + $(section).innerHeight();
+
+            if (displayBottom > sectionTop && displayTop < sectionBottom) {
+                $(section).css(animationCSS);
+                return false;
+            } else return true;
+        });
+    }
 }
