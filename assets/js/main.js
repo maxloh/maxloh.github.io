dab21228800d45e274977eb34c85baed4983c0a9
maxloh <35831069+maxloh@users.noreply.github.com>
2019-02-24 21:02:11 +0800

    Use a const variable to store selector strings

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 //CSS animation
-const animationCSS = { 'transition-duration': '1250ms', 'opacity': '1', 'transform': 'translateY(0)' };
+const animationCSS = { 'opacity': '1', 'transform': 'translateY(0)' };
 const delay = 400;
+const navbarSelector = '#navbar';
+const sectionSelector = 'main>.section';
 
 try {
     const animationObserver = new IntersectionObserver(function (entries, animationObserver) {
@@ -16,9 +18,9 @@ try {
     /*observe 'main>.section' and animate #navbar on page load*/
     $(document).ready(function () {
         /*If element with id #navbar exists, animate it*/
-        if ($('#navbar')[0]) $('#navbar').css(animationCSS);
+        if ($(navbarSelector)[0]) $(navbarSelector).css(animationCSS);
         setTimeout(function () {
-            document.querySelectorAll('main>.section').forEach(function (element) {
+            document.querySelectorAll(sectionSelector).forEach(function (element) {
                 animationObserver.observe(element);
             });
         }, delay);
@@ -31,17 +33,17 @@ catch (exception) {
     var sectionList;
 
     $(document).ready(function () {
-        if ($('#navbar')[0]) {
-            $('#navbar').css(animationCSS);
+        if ($(navbarSelector)[0]) {
+            $(navbarSelector).css(animationCSS);
         }
-        sectionList = $('main>.section');
+        sectionList = $(sectionSelector);
         setTimeout(addAnimation, delay);
     });
     $(window).scroll(addAnimation);
 
     function addAnimation() {
         let displayBottom = $(window).scrollTop() + $(window).height();
-        let displayTop = ($('#navbar')[0]) ? $(window).scrollTop() + $('#navbar').innerHeight() : $(window).scrollTop();
+        let displayTop = ($(navbarSelector)[0]) ? $(window).scrollTop() + $(navbarSelector).innerHeight() : $(window).scrollTop();
 
         sectionList = jQuery.grep(sectionList, function (section) {
             let sectionTranslateY = $(section).css('transform').match(/matrix\(.*, (\d*\.?\d+)\)/)[1];
