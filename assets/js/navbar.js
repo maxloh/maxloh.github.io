c25fe46d40aadd1f6c293d7746caa07d16caa645
maxloh <35831069+maxloh@users.noreply.github.com>
2019-03-28 20:06:30 +0800

    Change if else structure

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -21,22 +21,23 @@ addEventListener('DOMContentLoaded', function () {
 addEventListener('DOMContentLoaded', function () {
     var navbarObserver = new MutationObserver(function () {
         // if one of the .nav-link is active, meaning that navbar is sticking to top of the page
-        if (document.querySelector('a.nav-link.active')) {
-            if (deviceType === 'desktop') {
+        if (deviceType === 'desktop') {
+            if (document.querySelector('a.nav-link.active')) {
                 css(navbar, {
-                    'margin-left': 'var(--navbar-strech)',
                     // 8 dp shadow in material design
-                    'box-shadow': '0px 5px 5px -3px rgba(0, 0, 0, 0.2), 0px 8px 10px 1px rgba(0, 0, 0, 0.14), 0px 3px 14px 2px rgba(0, 0, 0, 0.12)'
+                    'box-shadow': '0px 5px 5px -3px rgba(0, 0, 0, 0.2), 0px 8px 10px 1px rgba(0, 0, 0, 0.14), 0px 3px 14px 2px rgba(0, 0, 0, 0.12)',
+                    'margin-left': 'var(--navbar-strech)'
                 });
             } else {
-                css(navbar, {
-                    // 4 dp shadow in material design
-                    'box-shadow': '0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12)'
-                });
+                css(navbar, { 'box-shadow': '', 'margin-left': '' });
             }
         } else {
-            css(navbar, { 'box-shadow': '' });
-            if (deviceType === 'desktop') css(navbar, { 'margin-left': '' });
+            if (document.querySelector('a.nav-link.active')) {
+                // 4 dp shadow in material design
+                css(navbar, { 'box-shadow': '0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12)' });
+            } else {
+                css(navbar, { 'box-shadow': '' });
+            }
         }
     });
     document.querySelectorAll('li.nav-item:first-child>a.nav-link').forEach(function (element) {
