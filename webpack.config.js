ca653d8cc5557c7a0887e7d4ef3821f9efafd047
maxloh <35831069+maxloh@users.noreply.github.com>
2019-07-02 20:51:42 +0800

    Use terser plugin to minify JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,20 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
-    entry: '.\\assets\\dev\\js\\main.js',
+    entry: './assets/dev/js/main.js',
     output: {
         filename: 'app.js',
-        path: path.resolve(__dirname, '.\\assets\\js\\'),
-    }
+        path: path.resolve(__dirname, './assets/js/'),
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({
+            sourceMap: true,
+            terserOptions: {
+                mangle: { reserved: ['css'] }
+            },
+        })],
+    },
+    mode: 'production',
+    devtool: 'source-map'
 };
