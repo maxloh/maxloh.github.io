d494252bf48bd54eb91d16b7f0010097f16b80d4
maxloh <35831069+maxloh@users.noreply.github.com>
2019-07-02 20:37:04 +0800

    [WIP] Using webpack to compile scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,33 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
-    entry: '.\\assets\\dev\\js\\main.js',
+    entry: ['./assets/dev/js/main.js', './assets/dev/scss/style.scss'],
     output: {
         filename: 'app.js',
-        path: path.resolve(__dirname, '.\\assets\\js\\'),
-    }
+        path: path.resolve(__dirname, './assets/js/'),
+    },
+    optimization: {
+        minimizer: [new TerserPlugin()],
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader",
+                {
+                    loader: 'resolve-url-loader',
+                    options: {}
+                },
+                "sass-loader"
+            ]
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "assets/css/style.css",
+        })
+    ]
 };
