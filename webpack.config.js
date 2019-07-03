6920c2892085356691422b1a00b6310454c91fd2
maxloh <35831069+maxloh@users.noreply.github.com>
2019-07-03 15:34:53 +0800

    [WIP] Use webpack to compile css

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,55 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './assets/dev/js/app.js',
+    entry: ['./assets/dev/js/app.js', './assets/dev/scss/style.scss'],
     output: {
         filename: 'app.bundle.js',
         path: path.resolve(__dirname, './assets/js/'),
     },
     mode: 'production',
-    devtool: 'source-map'
+    devtool: 'source-map',
+    module: {
+        rules: [{
+                test: /\.scss$/,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {}
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {}
+                    },
+                    {
+                        loader: 'resolve-url-loader',
+                        options: {}
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                            sourceMapContents: false
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|gif|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                }]
+            },
+            {
+                test: /\.(woff2)$/,
+                use: [{
+                    loader: 'file-loader',
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "assets/css/[name].css"
+        })
+    ]
 };
