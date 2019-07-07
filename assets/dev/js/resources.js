7eb8dfec5207688fa39daaec9d15489b9b980130
maxloh <35831069+maxloh@users.noreply.github.com>
2019-07-07 22:54:30 +0800

    Update code fetching resources.md

--- assets/dev/js/resources.js
+++ assets/dev/js/resources.js
@@ -2,11 +2,11 @@ import * as Remarkable from 'remarkable';
 import { css } from './functions';
 
 export const initResourcesList = () => {
-    fetch(`${location.origin}/assets/md/resources.md`).then((response) => {
-        response.text().then((text) => {
-            document.getElementById('resources-list').innerHTML = new Remarkable().render(text)
+    fetch(`${location.origin}/assets/md/resources.md`)
+        .then(response => response.text())
+        .then(text => {
+            document.getElementById('resources-list').innerHTML = new Remarkable().render(text);
         });
-    });
 
     document.getElementById('resources-btn').onclick = () => {
         css('.overlay', { 'visibility': 'visible', 'opacity': '1' });
