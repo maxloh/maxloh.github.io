85d653a505a54f57217ddd5bd382815b1cc9445a
maxloh <35831069+maxloh@users.noreply.github.com>
2019-07-11 23:55:28 +0800

    [WIP] Add Desktop navigation 4

--- assets/dev/js/navbar.js
+++ assets/dev/js/navbar.js
@@ -63,63 +63,65 @@ export const initNavbar = () => {
      */
 
     // Horizontal center of the page
-    const pageCenter = Math.ceil(window.innerWidth / 2);
     const getCurrentSection = () => {
         let currentSection;
+        let screenBottom = (deviceType === 'desktop') ? window.innerHeight - navbarHeight : window.innerHeight;
         for (let element of document.getElementsByTagName('section')) {
-            if (getSectionTop(element) <= window.innerWidth - navbarHeight) {
+            if (element.getBoundingClientRect().top <= screenBottom) {
                 currentSection = element;
             }
         }
-        return currentSection !== undefined ? currentSection : null;
+        return currentSection;
     };
-    const getSectionTop = (section) => getSectionTop(section) + parseFloat(getComputedStyle(section).marginTop);
     let previousScrollY = window.scrollY;
     let targetSection;
 
     addEventListener('scroll', (event) => {
         // Add scrollapy to page
-        if (deviceType === 'mobile' && getSectionTop(navbar) !== 0) return;
+        if (deviceType === 'mobile' && navbar.getBoundingClientRect().top !== 0) return;
         let activeLink = document.querySelector('a.nav-link.active');
         if (activeLink) activeLink.classList.remove('active');
         if (getCurrentSection()) document.querySelector('a.nav-link[href="#' + getCurrentSection().id + '"]').classList.add('active');
 
         // Scroll to next/previous section on scroll on desktop
         if (deviceType === 'desktop') {
-            setInterval(() => {
-                if (targetSection && getSectionTop(targetSection) === 0) css('body', { 'overflow': '' });
-            }, 10);
-            if (targetSection && getSectionTop(targetSection) !== 0) return;
+            // setInterval(() => {
+            //     if (targetSection && targetSection.getBoundingClientRect().top === 0) css('body', { 'overflow': '' });
+            // }, 10);
+            // if (targetSection && targetSection.getBoundingClientRect().top !== 0) return;
 
             let currentSection = getCurrentSection();
-            if (currentSection && getSectionTop(currentSection) >= 0) {
-                if (window.scrollY > previousScrollY) {
-                    console.log('scrolling down');
-                    let nextSection = currentSection.nextElementSibling;
-                    console.log(getSectionTop(nextSection) + ' ' + window.innerHeight);
-                    if (nextSection && getSectionTop(nextSection) < window.innerHeight - navbarHeight) {
-                        targetSection = nextSection;
-                        css('body', { 'overflow': 'hidden' });
-                        window.scroll({
-                            top: getSectionTop(nextSection),
-                            behavior: 'smooth'
-                        });
-                    }
-                } else {
-                    console.log('scrolling up');
-                    let previousSection = currentSection.previousElementSibling;
-                    console.log(previousSection.getBoundingClientRect().bottom);
-                    if (previousSection && previousSection.getBoundingClientRect().bottom > 0) {
-                        targetSection = previousSection;
-                        css('body', { 'overflow': 'hidden' });
-                        window.scroll({
-                            top: getSectionTop(previousSection),
-                            behavior: 'smooth'
-                        });
-                    }
-                }
-                previousScrollY = window.scrollY;
-            }
+            console.log(window.scrollY + ' ' + previousScrollY);
+            console.log(getCurrentSection());
+
+            // if (window.scrollY > previousScrollY) {
+            //     console.log('scrolling down');
+            //     let nextSection = currentSection.nextElementSibling;
+            //     console.log(nextSection);
+            //     console.log(nextSection.getBoundingClientRect().top + ' ' + window.innerHeight);
+            //     if (nextSection && nextSection.getBoundingClientRect().top < window.innerHeight - navbarHeight) {
+            //         targetSection = nextSection;
+            //         css('body', { 'overflow': 'hidden' });
+            //         window.scroll({
+            //             top: nextSection.getBoundingClientRect().top,
+            //             behavior: 'smooth'
+            //         });
+            //     }
+            // } else {
+            //     console.log('scrolling up');
+            //     let previousSection = currentSection.previousElementSibling;
+            //     console.log(previousSection);
+            //     console.log(previousSection.getBoundingClientRect().bottom);
+            //     if (previousSection && previousSection.getBoundingClientRect().bottom > 0) {
+            //         targetSection = previousSection;
+            //         css('body', { 'overflow': 'hidden' });
+            //         window.scroll({
+            //             top: previousSection.getBoundingClientRect().top,
+            //             behavior: 'smooth'
+            //         });
+            //     }
+            // }
+            previousScrollY = window.scrollY;
         }
     });
 }
