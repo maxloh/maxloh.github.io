c8e2655577510fcbfca18812a54aff1d40f6f6ec
maxloh <35831069+maxloh@users.noreply.github.com>
2020-08-14 02:01:12 +0800

    temp3

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './assets/dev/js/app.js',
@@ -10,27 +11,22 @@ module.exports = {
   mode: process.env.NODE_ENV,
   devtool: 'source-map',
   plugins: [
-    new webpack.BannerPlugin('Copyright (c) 2019 Loh Ka Hong. All rights reserved.')
+    new webpack.BannerPlugin('Copyright (c) 2019 Loh Ka Hong. All rights reserved.'),
+    new MiniCssExtractPlugin({ filename: '../css/style.css' }),
   ],
   module: {
     rules: [
       {
         test: /\.(scss)$/,
         use: [
-          'style-loader', // inject CSS to page
+          MiniCssExtractPlugin.loader,
           {
             loader: 'css-loader', // translates CSS into CommonJS modules
             options: { url: false }
           },
           {
             loader: 'postcss-loader', // Run postcss actions
-            options: {
-              plugins: function () { // postcss plugins, can be exported to postcss.config.js
-                return [
-                  require('autoprefixer')
-                ];
-              }
-            }
+            options: { plugins: () => require('autoprefixer') } // postcss plugins, can be exported to postcss.config.js
           },
           'sass-loader' // compiles Sass to CSS
         ]
