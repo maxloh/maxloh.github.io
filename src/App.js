def7f63705125ea71777a22e3b1a03a1fa4163cb
maxloh <35831069+maxloh@users.noreply.github.com>
2020-08-17 03:42:04 +0800

    feat: navbar and animated background

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from './components/Navbar.js';
+import Background from './components/Background.js';
 import './App.scss';
 
+const sectionContext = React.createContext();
+
 function App() {
+  const [platform, setPlatform] = React.useState('desktop');
+  const [currentSection, setCurrentSection] = React.useState('about-me');
+  const getPlatform = () => (matchMedia('(pointer:fine)').matches && window.innerWidth >= 768) ? 'desktop' : 'mobile';
+
+  window.addEventListener('resize', () => setPlatform(getPlatform()));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${platform}`}>
+      <sectionContext.Provider value={{ currentSection: currentSection, setCurrentSection: setCurrentSection }}>
+        <Navbar />
+      </sectionContext.Provider>
+      <Background />
     </div>
   );
 }
 
+
+export { sectionContext };
 export default App;
