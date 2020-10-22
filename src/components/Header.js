2d0ef5d741a44c308639c6bf964f4de4f3aa7bce
maxloh <35831069+maxloh@users.noreply.github.com>
2020-10-23 02:04:06 +0800

    feat(header): update contents

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,10 @@ import './Header.scss';
 
 function Header(props) {
   return (
-    <header className={props.position}>
-      Header
+    <header className={props.position} style={{ flexDirection: 'column', alignItems: 'start' }}>
+      <h1 className="greeting" style={{ fontSize: '8rem', fontWeight: 300 }}>Hi, I am Max Loh.</h1>
+      <h2 className="description" style={{ fontSize: '4rem', fontWeight: 300 }}>A Hong Kong based front end developer.</h2>
+      <div className="mouse-container"><div className="mouse"></div></div>
     </header>
   );
 }
