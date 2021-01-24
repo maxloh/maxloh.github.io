086c398b970d1978af279d2a1c02944a832ef859
maxloh <35831069+maxloh@users.noreply.github.com>
2021-01-24 22:22:45 +0800

    feat: remove intersection-observer polyfill, adds some kind of IE support

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,26 +1,33 @@
-import 'intersection-observer';
-
 export const initAnimation = () => {
     let delay = 0;
     const navbar = document.getElementById('navbar');
-    const section = document.getElementsByClassName('section');
+    const sections = document.getElementsByClassName('section');
 
-    const animationObserver = new IntersectionObserver((entries, animationObserver) => {
-        for (const entry of entries) {
-            // Animate element when it enter viewport
-            if (entry.intersectionRatio > 0) {
-                animationObserver.unobserve(entry.target);
-                setTimeout(() => {
-                    entry.target.classList.add('show');
-                    delay -= 500;
-                }, delay);
-                delay += 500;
+    try {
+        const animationObserver = new IntersectionObserver((entries, animationObserver) => {
+            for (const entry of entries) {
+                // Animate element when it enter viewport
+                if (entry.intersectionRatio > 0) {
+                    animationObserver.unobserve(entry.target);
+                    setTimeout(() => {
+                        entry.target.classList.add('show');
+                        delay -= 500;
+                    }, delay);
+                    delay += 500;
+                }
             }
-        }
-    });
+        });
 
-    // observe 'main>.section' and animate #navbar on page load
-    for (const element of [navbar, ...section]) {
-        animationObserver.observe(element);
+        // observe 'main>.section' and animate #navbar on page load
+        for (const element of [navbar, ...sections]) {
+            animationObserver.observe(element);
+        }
+    } catch (error) {
+        navbar.classList.add('show');
+        delay += 500;
+        for (let i = 0; i < sections.length; i++) {
+            setTimeout(() => sections[i].classList.add('show'), delay);
+            delay += 500;
+        }
     }
 };
