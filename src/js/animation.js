db4d46c33baef66e7a58dc5dab39006d67c8819b
maxloh <35831069+maxloh@users.noreply.github.com>
2021-02-17 02:52:09 +0800

    feat: new color palette 7

--- src/js/animation.js
+++ src/js/animation.js
@@ -7,15 +7,20 @@ export default function initAnimation() {
       (entries, animationObserver) => {
         let delay = firstRun ? 0 : 250;
 
-        // prettier-ignore
         // Animate element within the viewport
-        for (const entry of entries.filter(entry => entry.intersectionRatio > 0)) {
+        for (const entry of entries.filter(
+          entry => entry.intersectionRatio > 0
+        )) {
           animationObserver.unobserve(entry.target);
 
-          for (const child of entry.target.children) {
-            child.style.transitionDelay = `${delay}ms`;
-          }
-          entry.target.classList.add('show');
+          entry.target.style.setProperty(
+            '--section-transition-delay',
+            `${delay}ms`
+          );
+          entry.target.classList.remove('hide');
+          setTimeout(() => {
+            entry.target.style.removeProperty('--section-transition-delay');
+          }, delay + 100);
 
           // While animating multiple sections (e.g. on page load),
           // delay the animation of subsequent sections
@@ -25,19 +30,21 @@ export default function initAnimation() {
       }
     );
 
-    // prettier-ignore
-    for (const element of [document.getElementById('navbar'), ...document.getElementsByTagName('section')]) {
+    for (const element of [
+      document.getElementById('navbar'),
+      ...document.getElementsByTagName('section')
+    ]) {
       animationObserver.observe(element);
     }
   } catch (error) {
     let delay = 0;
     const sections = document.getElementsByTagName('section');
 
-    document.getElementById('navbar').classList.add('show');
+    document.getElementById('navbar').classList.remove('hide');
     delay += delayBetweenTransitions;
 
     for (let i = 0; i < sections.length; i++) {
-      setTimeout(() => sections[i].classList.add('show'), delay);
+      setTimeout(() => sections[i].classList.remove('hide'), delay);
       delay += delayBetweenTransitions;
     }
   }
